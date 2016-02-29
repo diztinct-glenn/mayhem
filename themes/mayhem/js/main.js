86269f60dd606e5883903a5e605557308d516d25
gBasgaard <glenn_basgaard@yahoo.com>
2016-02-29 17:22:55 +0100

    Fixed section in view classes.

--- themes/mayhem/js/main.js
+++ themes/mayhem/js/main.js
@@ -2,69 +2,97 @@ jQuery( function( $ ) {
 	
 	$( document ).foundation();
 	
-	// $(function(){
-  //   $('#navbar a').click(function () {
-  //       $('#navbar a').css('color', 'white');//don't forget to reset other inactive links
-  //    $(this).css('color', 'black');
-  //    });
- // 	});	
-	
 	$('.grid').masonry({
-	  // set itemSelector so .grid-sizer is not used in layout
-	  itemSelector: '.grid-item',
-	  // use element for option
-	  columnWidth: 0
+		itemSelector: '.grid-item',
+		columnWidth: 0
 	})
-
-	// $("#container").nested({selector: '.box'});
-	// $("#container").nested({minWidth: 100});
-	// $("#container").nested({minColumn: 1});
-	// $("#container").nested({gutter: 1});
-	// $("#container").nested({
-	//   resizeToFit: true,
-	//   resizeToFitOptions: { 
-	//     resizeAny: true
-	//   }
-	// });
 
 	$('a[rel="lightbox"]').fluidbox();
 	
+	mayhemMenu();
 
-$( window ).scroll(function(){
-
-		if (isScrolledIntoView('.news h1') === true) {
-				$('#a-news a').addClass('in-view');
-		} else {
-				$('#a-news a').removeClass('in-view');
-		}
+	$( window ).scroll(function() {
 		
-		if (isScrolledIntoView('.about h1') === true) {
-				$('#a-about a').addClass('in-view');
-		}
-		
-		if (isScrolledIntoView('.behind-the-scenes h1') === true) {
-				$('#a-behind-the-scenes a').addClass('in-view');
-		}
-		
-		if (isScrolledIntoView('.contact h1') === true) {
-				$('#a-contact a').addClass('in-view');
-		}
+		mayhemMenu();
 		
 	});
+	
+	function mayhemMenu() {
+		
+		$news_in_view = false;
+		$about_in_view = false;
+		$behind_the_scenes_in_view = false;
+		$contact_in_view = false;
+
+		if ( isScrolledIntoView('.news h1') && $news_in_view === false ) {
+			
+			$about_in_view = false;
+			$behind_the_scenes_in_view = false;
+			$contact_in_view = false;
+			
+			$('.top-bar a').removeClass('in-view');
+			
+			$('#a-news a').addClass('in-view');
+			$news_in_view = true;
+		}
+		
+		if (isScrolledIntoView('.about h1') ) {
+			
+			$news_in_view = false;
+			$behind_the_scenes_in_view = false;
+			$contact_in_view = false;
+			
+			$('.top-bar a').removeClass('in-view');
+			
+			$('#a-about a').addClass('in-view');
+			
+			$about_in_view = false;
+			
+		}
+		
+		if (isScrolledIntoView('.behind-the-scenes h1') ) {
+			
+			$news_in_view = false;
+			$about_in_view = false;
+			$contact_in_view = false;
+			
+			$('.top-bar a').removeClass('in-view');
+			
+			$('#a-behind-the-scenes a').addClass('in-view');
+			
+			$behind_the_scenes_in_view = false;
+			
+		}
+		
+		if (isScrolledIntoView('.contact h1') ) {
+			
+			$news_in_view = false;
+			$about_in_view = false;
+			$behind_the_scenes_in_view = false;
+			
+			$('.top-bar a').removeClass('in-view');
+			
+			$('#a-contact a').addClass('in-view');
+			
+			$contact_in_view = false;
+			
+		}
+		
+	}
 		
 	function isScrolledIntoView(elem) {
-	    var $elem = $(elem);
-	    var $window = $(window);
+		
+		var $elem = $(elem);
+		var $window = $(window);
 
-	    var docViewTop = $window.scrollTop();
-	    var docViewBottom = docViewTop + $window.height();
+		var docViewTop = $window.scrollTop();
+		var docViewBottom = docViewTop + $window.height();
 
-	    var elemTop = $elem.offset().top;
-	    var elemBottom = elemTop + $elem.height();
+		var elemTop = $elem.offset().top;
+		var elemBottom = elemTop + $elem.height();
 
-	    return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+		return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+	
 	}
-
-
 	
 } );
