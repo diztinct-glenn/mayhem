2f61356b0cc05b30ba565fb906cdc50d723f48f9
gBasgaard <glenn_basgaard@yahoo.com>
2016-03-10 14:09:05 -0500

    Added Smooth Scrolling to the Top + Fade Out "Carved in Mayhem" Text Overlay

--- themes/mayhem/parts/featured.php
+++ themes/mayhem/parts/featured.php
@@ -6,7 +6,18 @@
 			<source src="<?php echo get_template_directory_uri() ?>/vid/mayhem-background.webm" type="video/webm">
 		</video>
 		<div class="overlay">
-			<p> Carved in Mayhem </p>
+			<div class="top">
+				<div class="title">
+					<p> Carved in Mayhem </p>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="overlay-two">
+		<div class="overlay-stay">
+			<a href="#news">
+				<i class="fa fa-angle-down fa-3x arrow-nav hvr-hang"></i>
+			</a>
 		</div>
 	</div>
 </div>
